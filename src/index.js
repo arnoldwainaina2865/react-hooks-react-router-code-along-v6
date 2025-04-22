1b6b94301bbb993f5c3ef7992cf53819bd79395b
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-04-22 08:51:44 +0300

    phase2lab16

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
